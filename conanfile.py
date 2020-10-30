fe5ebfea1f42b6ee7e01c4bfcf2bbb4db9bd32d0
Steve Mokris <steve@kosada.com>
2020-10-30 10:17:23 -0400

    Build for both x86_64 + arm64. [#17836]

--- conanfile.py
+++ conanfile.py
@@ -23,11 +23,11 @@ class Ld64Conan(ConanFile):
     # This is the earliest version that supports tapi-1100.0.11.
     llvm_version = '11.0.0'
 
-    package_version = '2'
+    package_version = '3'
     version = '%s-%s' % (ld64_version, package_version)
 
     requires = 'llvm/5.0.2-1@vuo/stable'
-    build_requires = 'macos-sdk/11-0@vuo/stable'
+    build_requires = 'macos-sdk/11.0-0@vuo/stable'
     settings = 'os', 'compiler', 'build_type', 'arch'
     url = 'https://opensource.apple.com/'
     license = 'https://opensource.apple.com/source/ld64/ld64-%s/APPLE_LICENSE.auto.html' % ld64_version
@@ -36,7 +36,11 @@ class Ld64Conan(ConanFile):
     dyld_source_dir = 'dyld-%s' % dyld_version
     llvm_source_dir = 'llvm-%s.src' % llvm_version
     tapi_source_dir = '%s/projects/tapi-%s' % (llvm_source_dir, tapi_version)
-    llvm_build_dir = '_build_llvm'
+
+    build_llvm_x86_dir = '_build_llvm_x86'
+    build_llvm_arm_dir = '_build_llvm_arm'
+    install_universal_dir = '_install_universal'
+
     exports_sources = '*.patch'
 
     def source(self):
@@ -75,28 +79,70 @@ class Ld64Conan(ConanFile):
             tools.replace_in_file('ld64.xcodeproj/project.pbxproj',
                                   '				SDKROOT = macosx.internal;',
                                   '				SDKROOT = macosx;')
+            tools.replace_in_file('ld64.xcodeproj/project.pbxproj',
+                                  '				VALID_ARCHS = "x86_64 i386 ppc";',
+                                  '				VALID_ARCHS = "x86_64 arm64";')
+            tools.replace_in_file('ld64.xcodeproj/project.pbxproj',
+                                  '				MACOSX_DEPLOYMENT_TARGET = 10.14;',
+                                  '				MACOSX_DEPLOYMENT_TARGET = 10.11;')
 
         self.run('mv %s/APPLE_LICENSE %s/%s.txt' % (self.ld64_source_dir, self.ld64_source_dir, self.name))
 
     def build(self):
-        tools.mkdir(self.llvm_build_dir)
-        with tools.chdir(self.llvm_build_dir):
-            cmake = CMake(self)
-            cmake.definitions['CMAKE_OSX_SYSROOT'] = self.deps_cpp_info['macos-sdk'].rootpath
-            cmake.definitions['CMAKE_CXX_FLAGS']  = ' -I%s/../%s/projects/clang-%s.src/include' % (os.getcwd(), self.llvm_source_dir, self.llvm_version)
-            cmake.definitions['CMAKE_CXX_FLAGS'] += ' -I%s/projects/clang-%s.src/include' % (os.getcwd(), self.llvm_version)
-            cmake.definitions['LLVM_INCLUDE_TESTS'] = 'OFF'
-            cmake.definitions['CLANG_TABLEGEN_EXE'] = 'clang-tblgen'
+        cmake = CMake(self)
+        cmake.definitions['CMAKE_OSX_SYSROOT'] = self.deps_cpp_info['macos-sdk'].rootpath
+        cmake.definitions['CMAKE_CXX_FLAGS']  = ' -I%s/%s/projects/clang-%s.src/include' % (os.getcwd(), self.llvm_source_dir, self.llvm_version)
+        cmake.definitions['CMAKE_CXX_FLAGS'] += ' -I%s/%s/projects/clang-%s.src/include' % (os.getcwd(), self.build_llvm_x86_dir, self.llvm_version)
+        cmake.definitions['CMAKE_OSX_DEPLOYMENT_TARGET'] = '10.11'
+        cmake.definitions['LLVM_INCLUDE_TESTS'] = 'OFF'
+        cmake.definitions['LLVM_TARGETS_TO_BUILD'] = 'X86;AArch64'
+        cmake.definitions['CLANG_TABLEGEN_EXE'] = 'clang-tblgen'
+
+
+        self.output.info("=== Build libtapi for x86_64 ===")
+        cmake.definitions['CMAKE_OSX_ARCHITECTURES'] = 'x86_64'
+        cmake.definitions['LLVM_DEFAULT_TARGET_TRIPLE'] = 'x86_64-apple-macos10.11.0'
+        tools.mkdir(self.build_llvm_x86_dir)
+        with tools.chdir(self.build_llvm_x86_dir):
             cmake.configure(source_dir='../%s' % self.llvm_source_dir, build_dir='.')
             cmake.build(target='libtapi')
 
+
+        self.output.info("=== Build libtapi for arm64 ===")
+        cmake.definitions['CMAKE_CROSSCOMPILING'] = 'ON'
+        cmake.definitions['CMAKE_OSX_ARCHITECTURES'] = 'arm64'
+        cmake.definitions['LLVM_DEFAULT_TARGET_TRIPLE'] = 'arm64-apple-macos10.11.0'
+        cmake.definitions['LLVM_TARGET_ARCH'] = 'AArch64'
+        # tblgen needs to run on the host, so it should be x86 even when cross-compiling for arm.
+        cmake.definitions['LLVM_TABLEGEN'] = '%s/%s/bin/llvm-tblgen' % (os.getcwd(), self.build_llvm_x86_dir)
+        cmake.definitions['CLANG_TABLEGEN'] = '%s/%s/bin/clang-tblgen' % (os.getcwd(), self.build_llvm_x86_dir)
+        cmake.definitions['CLANG_TABLEGEN_EXE'] = cmake.definitions['CLANG_TABLEGEN']
+        flags = ' -target arm64-apple-macos10.11.0'
+        cmake.definitions['CMAKE_C_FLAGS'] = flags
+        cmake.definitions['CMAKE_CXX_FLAGS'] += flags
+        tools.mkdir(self.build_llvm_arm_dir)
+        with tools.chdir(self.build_llvm_arm_dir):
+            cmake.configure(source_dir='../%s' % self.llvm_source_dir, build_dir='.')
+            cmake.build(target='libtapi')
+
+
+        self.output.info("=== Merge x86_64 + arm64 libtapi ===")
+        tools.mkdir(self.install_universal_dir)
+        with tools.chdir(self.install_universal_dir):
+            tools.mkdir('lib')
+            with tools.chdir('lib'):
+                self.run('lipo -create ../../%s/lib/libtapi.dylib ../../%s/lib/libtapi.dylib -output libtapi.dylib' % (self.build_llvm_x86_dir, self.build_llvm_arm_dir))
+
+
+        self.output.info("=== Build ld for both x86_64 + arm64 ===")
         with tools.chdir(self.ld64_source_dir):
             tools.replace_in_file('ld64.xcodeproj/project.pbxproj',
                                   '"-ltapi",',
-                                  '"%s/../%s/lib/libtapi.dylib",' % (os.getcwd(), self.llvm_build_dir))
-            self.run('RC_SUPPORTED_ARCHS="x86_64 arm64" xcodebuild -target ld HEADER_SEARCH_PATHS="../%s/include ../%s/include ../%s/include ../%s/projects/tapi-%s/include src/ld"' % (self.dyld_source_dir, self.llvm_source_dir, self.tapi_source_dir, self.llvm_build_dir, self.tapi_version))
+                                  '"%s/../%s/lib/libtapi.dylib",' % (os.getcwd(), self.install_universal_dir))
+            self.run('RC_SUPPORTED_ARCHS="x86_64 arm64" xcodebuild ARCHS="x86_64 arm64" -target ld HEADER_SEARCH_PATHS="../%s/include ../%s/include ../%s/include ../%s/projects/tapi-%s/include src/ld"' % (self.dyld_source_dir, self.llvm_source_dir, self.tapi_source_dir, self.build_llvm_x86_dir, self.tapi_version))
+
 
     def package(self):
         self.copy('ld', src='%s/build/Release-assert' % self.ld64_source_dir, dst='bin')
-        self.copy('libtapi.dylib', src='%s/lib' % self.llvm_build_dir, dst='lib')
+        self.copy('libtapi.dylib', src='%s/lib' % self.install_universal_dir, dst='lib')
         self.copy('%s.txt' % self.name, src=self.ld64_source_dir, dst='license')
